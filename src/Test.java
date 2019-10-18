1b36947c538bf97ac28a6f0afa98e1b201f356f9
ezelev <iiezelev@gmail.com>
2019-10-18 16:31:55 +0300

    add russian encoding of devices names

--- src/Test.java
+++ src/Test.java
@@ -2,20 +2,24 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Mixer;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 
 public class Test {
 
     public static void main(String[] args) throws UnsupportedEncodingException {
 
-            System.out.println(("Audio mixer info: "));
-            PrintStream ps = new PrintStream(System.out, true, "UTF-8");
-            Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();
-            for(int i = 0; i < mixerInfo.length; i++)
-            {
-                ps.println(mixerInfo[i].getName());
-            }
+        Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();
+        for (Mixer.Info info : mixerInfo) {
+            String deviceName = info.getName();
 
+            byte[] bytes = deviceName.getBytes(StandardCharsets.ISO_8859_1); // <- from
+            String value = new String(bytes, "Windows-1251"); // <- to
+
+            System.out.println(value);
+
+        }
 
     }
+
 }
